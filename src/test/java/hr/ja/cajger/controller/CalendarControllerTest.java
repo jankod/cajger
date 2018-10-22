5bcbe3c69580ed2d75d969307a9b0dfe385b50a0
Janko <jdiminic@gmail.com>
2018-10-22 10:50:09 +0200

    commit all

--- src/test/java/hr/ja/cajger/controller/CalendarControllerTest.java
+++ src/test/java/hr/ja/cajger/controller/CalendarControllerTest.java
@@ -7,12 +7,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.client.RestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -32,10 +40,22 @@ public class CalendarControllerTest {
 
     @Test
     public void testEvents() throws Exception {
-//        mockMvc.perform(get("/cal/events")
-//                .contentType(MediaType.APPLICATION_JSON_UTF8)
-//        ).andExpect(status().isOk())
-//                .andExpect(content().string(""));
+        mockMvc.perform(get("/cal/events")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+        ).andExpect(status().isOk())
+                .andExpect(content().string(""));
+        // TODO finish
+        mockMvc.perform(get("/cal/events"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].description", is("Lorem ipsum")))
+                .andExpect(jsonPath("$[0].title", is("Foo")))
+                .andExpect(jsonPath("$[1].id", is(2)))
+                .andExpect(jsonPath("$[1].description", is("Lorem ipsum")))
+                .andExpect(jsonPath("$[1].title", is("Bar")));
+
 
     }
 }
